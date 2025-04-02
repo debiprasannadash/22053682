f745a7ca73d705e49cce6409e939e111968a79f6
debiprasannadash <dp.dash12@gmail.com>
2025-04-02 19:29:05 +0530

    Further Changes

--- app/Components/Avg_cal.js
+++ app/Components/Avg_cal.js
@@ -2,20 +2,35 @@
 import React,{ useState} from 'react'
 
 export default function Avg_cal(){
+    const [numberType, setNumberType]=useState("p")
+    const [prevState, setPrevState] = useState([])
+    const [currState, setCurrState] = useState([])
+    const [average, setAverage] = useState(null)
+
+    const apiurls = {
+        p:"http://20.244.56.144/evaluation-service/primes",
+        f:"http://20.244.56.144/evaluation-service/fibo",
+        e:"http://20.244.56.144/evaluation-service/even",
+        r:"http://20.244.56.144/evaluation-service/rand"
+    };
     
     return(
-        <div>
+        <div className="">
             <div className=" mt-[4%] flex justify-center">
                 <h1 className=" text-4xl ">Average Calculator</h1>
             </div>
-            <div className="mt-[4%]">
+
+            <div className="mt-[4%] flex justify-center">
             <select className=" border rounded">
                     <option>Prime Numbers</option>
                     <option>Fibonacci Numbers</option>
                     <option>Even Numbers</option>
                     <option>Random Numbers</option>
                 </select>
-                
+            </div>
+
+            <div className="mt-[4%] flex justify-center">
+                <buttton className=" rounded bg-amber-500 hover:bg-green-400">Fetch nos.</buttton>
             </div>
         </div>
     )
