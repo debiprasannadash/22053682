6f058ab4490c3f1e6fe9c334d6f6f72ad8e29a5e
debiprasannadash <dp.dash12@gmail.com>
2025-04-02 19:08:31 +0530

    Changes made

---
+++ app/Components/Avg_cal.js
@@ -0,0 +1,21 @@
+"use client"
+import React,{ useState} from 'react'
+
+export default function Avg_cal(){
+    
+    return(
+        <div>
+            <div className=" mt-[4%] flex justify-center">
+                <h1 className=" text-4xl ">Average Calculator</h1>
+            </div>
+            <div className="mt-[4%]">
+            <select>
+                    <option>Prime Numbers</option>
+                    <option>Fibonacci Numbers</option>
+                    <option>Even Numbers</option>
+                    <option>Random Numbers</option>
+                </select>
+            </div>
+        </div>
+    )
+}
